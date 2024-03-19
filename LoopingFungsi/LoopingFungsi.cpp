d093912e31fab0f33706c4ea731606be7ae76923
finanovita <finanovita479@gmail.com>
2024-03-19 10:16:34 +0700

    Add project files.

---
+++ LoopingFungsi/LoopingFungsi.cpp
@@ -0,0 +1,12 @@
+// LoopingFungsi.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int i;
+	int arr[5];
+}
+
